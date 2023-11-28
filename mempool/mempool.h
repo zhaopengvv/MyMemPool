88e17df4f349d32b9ec3c46a28c8bb2336388fbb
zhaopengvv <zhaopengvv@163.com>
2023-11-28 23:44:48 +0800

    complete mempool v1.0

---
+++ mempool/mempool.h
@@ -0,0 +1,26 @@
+#ifndef MEM_POOL_h
+#define MEM_POOL_H
+#include <cstdlib>
+
+#define MEM_BLOCK_INIT_SIZE (8 * 1024)
+#define MEM_BLOCK_MAX_SIZE  (8 * 1024 * 1024)
+#define MEM_FREELISTS_NUM 11
+#define MEM_MINBITS 3 /* smallest chunk size is 8 bytes */
+#define MEM_CHUNK_LIMIT (1 << (MEM_FREELISTS_NUM - 1 + MEM_MINBITS))
+
+#define MEM_BLOCK_HEAD_SIZE sizeof(MemBlockHead);
+#define MEM_CHUNK_HEAD_SIZE sizeof(MemChunkHead);
+
+struct MemBlockHead {
+    MemBlockHead *prev;
+    MemBlockHead *next;
+    char *freePtr;  /* start of free sapce in this block */
+    char *endptr;   /* end of space in this block */
+};
+
+struct MemChunkHead {
+    size_t size;
+    void *ptr;     /* ptr is the owning body if allocated, or the freelist link if free */
+};
+
+#endif
---
+++ mempool/mempoolmanager.h
@@ -0,0 +1,87 @@
+#ifndef MEM_POOL_MANAGER_H
+#define MEM_POOL_MANAGER_H
+
+#include <cstdlib>
+#include "mempool.h"
+
+#define UPDATE_NEXT_BLOCK_SIZE()                \
+    if (nextBlockSize < MEM_BLOCK_MAX_SIZE) {   \
+        nextBlockSize = nextBlockSize << 1;     \
+    }
+
+#define INSERT_BLOCK_INTO_LIST_HEAD(block)      \
+    do {                                        \
+        blocks_head->next->prev = block;        \
+        block->next = blocks_head->next;        \
+        block->prev = blocks_head;              \
+        blocks_head->next = block;              \
+    } while (true)
+
+#define INSERT_BLOCK_INTO_LIST_TAIL(block)      \
+    do {                                        \
+        blocks_head->prev->next = block;        \
+        block->prev = blocks_head->prev;        \
+        block->next = blocks_head;              \
+        blocks_head->prev = block;              \
+    } while (true)
+
+#define DELETE_BLOCK_FROM_LIST(block)           \
+    if (block != blocks_head) {                 \
+        MemBlockHead *prev = block->prev;       \
+        block->prev->next = block->next;        \
+        block->next->prev = prev;               \
+    }
+
+
+size_t GetAllocSize(size_t size)
+{
+    size_t alloc_size = 1 << MEM_MINBITS;
+    while (alloc_size < size) {
+        alloc_size = alloc_size << 1;
+    }
+    return alloc_size;
+}
+
+class MemPoolManager {
+private:
+    MemPoolManager() 
+    {
+        initBlockSize = MEM_BLOCK_INIT_SIZE;
+        nextBlockSize = MEM_BLOCK_INIT_SIZE * 2;
+        maxBlockSize = MEM_BLOCK_MAX_SIZE;
+        allocChunkLimit = MEM_CHUNK_LIMIT;
+
+        InitBlocks();
+    }
+    ~MemPoolManager() 
+    {
+        ReleaseBlocks();
+    }
+    
+    void InitBlocks();
+    void ReleaseBlocks();
+    int GetFreeIndex(size_t size);
+    MemBlockHead *MallocMemBlock(size_t size);
+    MemChunkHead *AllocMemChunkFromFreeList(size_t size);
+    MemChunkHead *AllocMemChunkFromBlock(MemBlockHead *block, size_t alloc_size);
+
+public:
+    void *MemAlloc(size_t size);
+    void MemFree(char *ptr);
+
+private:
+    MemBlockHead *blocks_head;
+    /**
+     * 数组默认长度为11，每个元素作为链表头指向一个由特定大小MemChunk组成的链表
+     * 第k个元素MemChunk的大小（字节）：2^(k + 2)，最小为8字节，最大不超过8K字节。
+    */
+    MemChunkHead freeList[MEM_FREELISTS_NUM];
+
+    size_t initBlockSize;
+    size_t maxBlockSize;
+    size_t nextBlockSize;
+    size_t allocChunkLimit;
+};
+
+
+#endif
